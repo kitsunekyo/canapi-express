2e9a27ca74fbe8980a5d1ee24044b47bed9a932c
kitsunekyo <alexander.spieslechner@gmail.com>
2018-05-11 15:24:57 +0200

    remove gitignore

---
+++ scripts/development/config.js
@@ -0,0 +1,10 @@
+// PROD config
+const CONFIG = {
+  DB_HOST: 'mongodb://localhost:27017/probes',
+  DB_PORT: 4000,
+  HOST: 'http://localhost:8080',
+  PORT: 8080,
+  API_HOST: 'http://localhost:8080/api',
+};
+
+module.exports = CONFIG;
